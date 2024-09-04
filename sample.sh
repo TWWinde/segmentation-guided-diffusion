031c2aaf6d41ccee25ccf8bf13513f5f8aa50819
TWWinde <twwinde@gmail.com>
2024-09-04 23:01:01 +0200

    sample

---
+++ sample.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=segguiddiff
+#SBATCH --output=segguiddiff%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1-23:00:00
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+#SBATCH --nodelist=linse21
+# SBATCH --qos=shortbatch
+# SBATCH --partition=highperf
+#SBATCH --gpus=rtx_a5000:1
+#SBATCH --nodelist=linse19
+
+
+# Activate everything you need
+
+#conda activate /anaconda3/envs/myenv
+module load cuda
+pyenv activate myenv38 #myenv #venv
+pip uninstall nvidia_cublas_cu11
+nvcc --version
+python -c "import torch; print(torch.__version__)"
+export CUDA_LAUNCH_BLOCKING=1
+
+# Run your python code
+
+CUDA_VISIBLE_DEVICES=0 python3 main.py \
+    --mode eval_many \
+    --model_type DDIM \
+    --img_size 256 \
+    --num_img_channels 1 \
+    --dataset "AutoPET" \
+    --eval_batch_size 8 \
+    --eval_sample_size 320 \
+    --seg_dir "/data/private/autoPET/autopet_2d/mask" \
+    --segmentation_guided \
+    --num_segmentation_classes 37
