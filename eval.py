e9ebeb8c6b3f54888f7dc93e185e6bdf5be46353
TWWinde <twwinde@gmail.com>
2024-09-08 22:40:02 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -81,6 +81,8 @@ def evaluate_sample_many(
                     img_fname = "{}/condon_{}".format(sample_dir, i)
                 else:
                     img_fname = f"{sample_dir}/{num_sampled + i:04d}.png"
+
+                img = (img * 255).astype(np.uint8)
                 img = Image.fromarray(img)
                 img.save(img_fname)
 
