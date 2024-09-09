fe45a062325b71cd76c46843425a95c2cbc92f19
TWWinde <twwinde@gmail.com>
2024-09-09 21:41:09 +0200

    Create metrics.sh

---
+++ metrics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=segguiddiff
+#SBATCH --output=segguiddiff%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1-23:00:00
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+#SBATCH --nodelist=linse21
+# SBATCH --qos=shortbatch
+# SBATCH --partition=highperf
+#SBATCH --gpus=rtx_a5000:1
+#SBATCH --nodelist=linse19
+
+
+
+
+python metrics.py
