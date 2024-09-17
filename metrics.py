a9d67e76d7e9aba2d999f290b89a0ba5d6b3df8f
TWWinde <twwinde@gmail.com>
2024-09-17 23:24:09 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -166,6 +166,8 @@ def compute_metrics_3d_our_model(root_path):
             input2 = np.expand_dims(input2, axis=0)
             print(input1.shape)
             print(input2.shape)
+        else:
+            print("scheiße")
 
             for i in range(input1.shape[2]):
                 input3 = torch.tensor(input1[:, :, i, :, :], dtype=torch.float32).to('cuda:0')
