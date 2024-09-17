c64825a50046be08ba8138cf83778d00fb3cfc93
TWWinde <twwinde@gmail.com>
2024-09-17 23:23:08 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -11,10 +11,10 @@
 #SBATCH --gpus=1
 #SBATCH --qos=batch
 #SBATCH --nodelist=linse21
-# SBATCH --qos=shortbatch
-# SBATCH --partition=highperf
-#SBATCH --gpus=rtx_a5000:1
-#SBATCH --nodelist=linse19
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
 
 
 # Activate everything you need
